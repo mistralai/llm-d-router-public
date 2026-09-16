hasB4K/llm-d-router

feat(coordinator): forward response headers across phases
Allow the pipeline to relay configured upstream response headers to later phase requests.

Signed-off-by: Mathis Felardos <mathis@mistral.ai>

pkg/coordinator/steps/prefill_decode_flow_test.go
@@ -183,3 +183,74 @@ func TestKVTransferParams_FlowFromPrefillToDecode(t *testing.T) {
 		t.Fatalf("decode sent wrong peer_host: %v", decodeReceivedKVParams["peer_host"])
 	}
 }
+
+func TestResponseHeaders_FlowFromPrefillToDecode(t *testing.T) {
+	var decodeHeaders http.Header
+
+	gwServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Header.Get(gateway.EPPProfileHeader) {
+		case gateway.PhasePrefill:
+			w.Header().Set("X-LLM-D-Disagg-Revision", "revision-b")
+			w.Header().Set("X-Disagg-Slice", "nvl72-domain-2")
+			w.Header().Set("X-Worker-Only", "must-not-be-forwarded")
+			_ = json.NewEncoder(w).Encode(map[string]any{"kv_transfer_params": map[string]any{}})
+
+		case gateway.PhaseDecode:
+			decodeHeaders = r.Header.Clone()
+			_ = json.NewEncoder(w).Encode(map[string]any{
+				"choices": []map[string]any{
+					{"message": map[string]any{"role": "assistant", "content": "done"}},
+				},
+			})
+
+		default:
+			http.Error(w, "not found", http.StatusNotFound)
+		}
+	}))
+	defer gwServer.Close()
+
+	gwClient := gateway.New(config.GatewayConfig{Address: gwServer.URL})
+	prefillStep, err := NewPrefillStep(gwClient, map[string]any{})
+	if err != nil {
+		t.Fatalf("build prefill step: %v", err)
+	}
+
+	reqCtx := &pipeline.RequestContext{
+		RequestID:    "test-header-flow",
+		OriginalPath: gateway.PathChatCompletions,
+		OriginalHeaders: http.Header{
+			"X-LLM-D-Disagg-Revision": {"client-revision"},
+			"X-Disagg-Slice":          {"client-slice"},
+		},
+		Body: map[string]any{
+			"model":    "llama-3",
+			"messages": []any{map[string]any{"role": "user", "content": "hello"}},
+		},
+	}
+
+	decodeStep, err := NewDecodeStep(gwClient, map[string]any{})
+	if err != nil {
+		t.Fatalf("build decode step: %v", err)
+	}
+	reqCtx.ResponseWriter = httptest.NewRecorder()
+	p, err := pipeline.NewWithForwardResponseHeaders(
+		[]pipeline.Step{prefillStep, decodeStep},
+		[]string{"X-LLM-D-Disagg-Revision", "x-disagg-slice"},
+	)
+	if err != nil {
+		t.Fatalf("build pipeline: %v", err)
+	}
+	if err := p.Execute(context.Background(), reqCtx); err != nil {
+		t.Fatalf("pipeline failed: %v", err)
+	}
+
+	if got := decodeHeaders.Get("X-LLM-D-Disagg-Revision"); got != "revision-b" {
+		t.Errorf("decode revision header = %q, want %q", got, "revision-b")
+	}
+	if got := decodeHeaders.Get("X-Disagg-Slice"); got != "nvl72-domain-2" {
+		t.Errorf("decode slice header = %q, want %q", got, "nvl72-domain-2")
+	}
+	if got := decodeHeaders.Get("X-Worker-Only"); got != "" {
+		t.Errorf("unconfigured response header was forwarded: %q", got)
+	}
+}

mistralai/llm-d-router-public

(cherry picked from commit 2aba73739731913764c1d47dee35fa4cf621a977)
(cherry picked from commit bf033bde4f126e36cd72b620479022425e15cbd3)

pkg/coordinator/steps/prefill_decode_flow_test.go
@@ -184,3 +184,74 @@ func TestKVTransferParams_FlowFromPrefillToDecode(t *testing.T) {
 		t.Fatalf("decode sent wrong peer_host: %v", decodeReceivedKVParams["peer_host"])
 	}
 }
+
+func TestResponseHeaders_FlowFromPrefillToDecode(t *testing.T) {
+	var decodeHeaders http.Header
+
+	gwServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Header.Get(gateway.EPPProfileHeader) {
+		case gateway.PhasePrefill:
+			w.Header().Set("X-LLM-D-Disagg-Revision", "revision-b")
+			w.Header().Set("X-Disagg-Slice", "nvl72-domain-2")
+			w.Header().Set("X-Worker-Only", "must-not-be-forwarded")
+			_ = json.NewEncoder(w).Encode(map[string]any{"kv_transfer_params": map[string]any{}})
+
+		case gateway.PhaseDecode:
+			decodeHeaders = r.Header.Clone()
+			_ = json.NewEncoder(w).Encode(map[string]any{
+				"choices": []map[string]any{
+					{"message": map[string]any{"role": "assistant", "content": "done"}},
+				},
+			})
+
+		default:
+			http.Error(w, "not found", http.StatusNotFound)
+		}
+	}))
+	defer gwServer.Close()
+
+	gwClient := gateway.New(config.GatewayConfig{Address: gwServer.URL})
+	prefillStep, err := NewPrefillStep(gwClient, map[string]any{})
+	if err != nil {
+		t.Fatalf("build prefill step: %v", err)
+	}
+
+	reqCtx := &pipeline.RequestContext{
+		RequestID:    "test-header-flow",
+		OriginalPath: reqcommon.PathChatCompletions,
+		OriginalHeaders: http.Header{
+			"X-LLM-D-Disagg-Revision": {"client-revision"},
+			"X-Disagg-Slice":          {"client-slice"},
+		},
+		Body: map[string]any{
+			"model":    "llama-3",
+			"messages": []any{map[string]any{"role": "user", "content": "hello"}},
+		},
+	}
+
+	decodeStep, err := NewDecodeStep(gwClient, map[string]any{})
+	if err != nil {
+		t.Fatalf("build decode step: %v", err)
+	}
+	reqCtx.ResponseWriter = httptest.NewRecorder()
+	p, err := pipeline.NewWithForwardResponseHeaders(
+		[]pipeline.Step{prefillStep, decodeStep},
+		[]string{"X-LLM-D-Disagg-Revision", "x-disagg-slice"},
+	)
+	if err != nil {
+		t.Fatalf("build pipeline: %v", err)
+	}
+	if err := p.Execute(context.Background(), reqCtx); err != nil {
+		t.Fatalf("pipeline failed: %v", err)
+	}
+
+	if got := decodeHeaders.Get("X-LLM-D-Disagg-Revision"); got != "revision-b" {
+		t.Errorf("decode revision header = %q, want %q", got, "revision-b")
+	}
+	if got := decodeHeaders.Get("X-Disagg-Slice"); got != "nvl72-domain-2" {
+		t.Errorf("decode slice header = %q, want %q", got, "nvl72-domain-2")
+	}
+	if got := decodeHeaders.Get("X-Worker-Only"); got != "" {
+		t.Errorf("unconfigured response header was forwarded: %q", got)
+	}
+}